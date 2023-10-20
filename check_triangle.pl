8975a381228280dd9aee6b2139a77418657d1b1f
Avariq Fazlur Rahman <91586313+avariqfr30@users.noreply.github.com>
2023-10-21 00:28:29 +0700

    Delete check_triangle.pl

--- check_triangle.pl
+++
@@ -1,7 +0,0 @@
-is_right_triangle(Angle1, Angle2, Angle3) :-
-    Angle1 + Angle2 + Angle3 =:= 180, % Check if the sum of all angles is 180 degrees
-    (Angle1 =:= 90 ; Angle2 =:= 90 ; Angle3 =:= 90). % Check if one of the angles is 90 degrees
-
-% Example usage:
-% is_right_triangle(90, 45, 45). % This is a right triangle.
-% is_right_triangle(60, 60, 60). % This is not a right triangle.
